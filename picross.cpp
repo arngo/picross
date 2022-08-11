6f89102263796b7275b6a3eff7f7da03f5e47815
arngo <27396817+arngo@users.noreply.github.com>
2022-08-11 14:29:22 -0400

    fix valid end states and add more test cases

--- picross.cpp
+++ picross.cpp
@@ -12,21 +12,22 @@ bool isValidRow(std::vector<char>& row, std::vector<int>& hints) {
                 continue;
             }
         }
-        if (curr_sum == *curr_hint && *e != 'o') {
+        if (curr_sum == *curr_hint && *e != 'o') { // empty cell found after current hint fulfilled
             curr_sum = 0;
             curr_hint++;
-        } else if (curr_sum < *curr_hint && *e == 'o') {
+        } else if (curr_sum < *curr_hint && *e == 'o') { // count filled cell for current hint
             curr_sum++;
-            if (curr_sum < *curr_hint && row.end()-e == 1) {
+            if (curr_sum < *curr_hint && row.end()-e == 1) { // end of row reached before filling hint
                 return false;
             }
-        } else if (curr_sum == 0 && *e != 'o' && row.end()-e > 1) {
+        } else if (curr_sum == 0 && *e != 'o' && row.end()-e > 1) { // empty cell found before end of row and between hints
             continue;
         } else {
             return false;
         }
     }
-    return hints.end() - curr_hint == 1;
+    // if 'o' found at end, curr_hint end up at the last hint rather than incrementing to hints.end()
+    return curr_hint == hints.end() || (curr_sum == *curr_hint && hints.end() - curr_hint == 1);
 }
 
 bool isValidSolution(std::vector<std::vector<char>> board, std::vector<std::vector<int>> row_hints, std::vector<std::vector<int>> col_hints) {
@@ -61,14 +62,20 @@ int main(int argc, char *argv[]) {
     }
     std::vector<std::vector<char>> rows = {
         {'o','x','o','o','o','o','o','x','o','o'}, // 1 5 2
-        {'o','o','o','o','o','o','o','x','o','o'}, // 7 2
         {'o','x','x','x','o','o','o','o','o','x','x','x','o','o'}, // 1 5 2
-        {'o','x','x','x','o','o','x','o','o','x','x','x','o','o'}, // 1 2 2 2
         {'o','x','o','o','o','o','o','x','o','o','x','o'}, // 1 5 2 1
-        {'x','x','x'}, // 0
+        {'o','x','x','x','o','o','x','o','o','x','x','x','o','o'}, // 1 2 2 2
+        {'o','o','o','o','o','o','o','x','o','o'}, // 7 2
+        {'o','x','o'}, // 1 1
+        {'o','x','x','o'}, // 1 1
         {'o','o','o'}, // 3
+        {'x','x','x','o'}, // 1
+        {'o','x','x','x'}, // 1
+        {'x','o','x'}, // 1
         {'o'}, // 1
+        {'x','x','x'}, // 0
         {'x'}, // 0
+        {'.',' ','x'}, // 0
     };
     std::cout << "Hint:";
     for (auto &e : hint) {
